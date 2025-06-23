aee022fa7b47771b1135191702fa16cfe7f815ad
covima01 <covima01@gmail.com>
2025-06-23 06:31:55 -0400

    Movement update

--- player.py
+++ player.py
@@ -22,11 +22,11 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        self.move(dt)
     def move(self, dt):
         keys = pygame.key.get_pressed()
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
         if keys[pygame.K_w]:
-            self.move(dt)
+            self.position += forward * PLAYER_SPEED * dt
         if keys[pygame.K_s]:
-            self.move(dt)
+            self.position -= forward * PLAYER_SPEED * dt
